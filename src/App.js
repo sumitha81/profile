8cf9a730ca949ce91ded7ca538aed9bf67d2c69e
sumitha81 <sumitha08aids@gmail.com>
2025-01-31 10:08:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
